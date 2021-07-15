95ce2fda159f28919109bb64f68ca39fc17eb446
Ali Nategh <a.nategh76@gmail.com>
2021-07-15 17:14:42 +0430

    Add project files.

---
+++ Auth.Api/Controllers/StartController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Api.Controllers
+{
+    [Route("[controller]")]
+    public class StartController : ControllerBase
+    {
+        public StartController() { }
+
+        [HttpGet]
+        public IActionResult StartTest()
+        {
+            return Ok("Api is working.");
+        }
+    }
+}
